126d96a02b97735793d4d70143f6a56c70c675c1
aaronk85 <aaronkenez@gmail.com>
2018-02-25 08:55:32 +1100

    adding library

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const maxItemInArray = (array) => Math.max.apply( Math, array )
+const minItemInArray = (array) => Math.min.apply( Math, array )
+
+module.exports = {
+  maxItemInArray,
+  minItemInArray
+}
